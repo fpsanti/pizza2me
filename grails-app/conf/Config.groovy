c87ceaa41d5c71a42b4e79dfbb8b6a24f4822dc6
selva.andre <andrea@sempai.(none)>
2012-03-01 00:10:32 +0100

    Added spring-security.core plugin and initialized User and Role security classes

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -91,3 +91,8 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+// Added by the Spring Security Core plugin:
+grails.plugins.springsecurity.userLookup.userDomainClassName = 'com.pizza2me.User'
+grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'com.pizza2me.UserRole'
+grails.plugins.springsecurity.authority.className = 'com.pizza2me.Role'
